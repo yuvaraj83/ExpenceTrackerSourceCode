7954b8cde35deaaf5d95bf819e5365be433e7651
your github yuvaraj83 <your github yuvarajponraj133@gmail.com>
2023-04-14 09:12:04 +0530

    second commit

---
+++ src/components/Transaction.js
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
+const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? "-" : "+";
+
+  return (
+    // <li className="minus">
+    //   {transaction.text}{" "}
+    //   <span>
+    //     {sign}${Math.abs(transaction.amount)}
+    //   </span>
+    //   <button className="delete-btn">X</button>
+    // </li>
+
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
+      {transaction.text}{" "}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+      </span>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        X
+      </button>
+    </li>
+  );
+};
+
+export default Transaction;
---
+++ src/components/TransactionList.js
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import Transaction from "./Transaction";
+
+const TransactionList = () => {
+  //const context = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
+
+  //console.log(context.transactions);
+
+  return (
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TransactionList;
